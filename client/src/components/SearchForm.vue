37947805743b3526ea828dbf26e4152dd65ca755
Valentin Kaelin <kalanehd@gmail.com>
2019-04-16 21:28:42 +0900

    Fix homepage

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -17,7 +17,14 @@
           <v-icon name="caret-down" class="ml-1"></v-icon>
         </div>
       </div>
-      <transition name="bounce">
+      <transition
+        enter-active-class="transition-all transition-fastest ease-out-quad"
+        leave-active-class="transition-all transition-faster ease-in-quad"
+        enter-class="opacity-0 scale-70"
+        enter-to-class="opacity-100 scale-100"
+        leave-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-70"
+      >
         <div
           v-if="dropdown"
           @click="dropdown = !dropdown"
@@ -27,7 +34,7 @@
             v-for="(region, index) in regions" 
             :key="region"
             @click="selectedRegion = region"
-            class="px-4 py-1 text-sm bg-teal-600 hover:bg-teal-500"
+            class="px-4 py-1 text-xs bg-teal-600 hover:bg-teal-500"
             :class="classRegions(index)"
           >
             {{ region }}
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="homepage h-screen flex flex-col items-center justify-center">
 
+    <div class="absolute top-0 right-0">
+      <div class="line line-top relative mr-4 mt-4 w-20 h-2"></div>
+    </div>
+
+    <div class="absolute bottom-0 left-0">
+      <div class="line line-bottom relative ml-4 mb-4 w-20 h-2"></div>
+    </div>
+
     <div class="relative flex flex-col items-center w-full max-w-lg">
-      <div class="absolute logo">
-        <img src="@/assets/Logo.svg" alt="logo">
-      </div>
+      <img class="absolute logo" src="@/assets/Logo.svg" alt="logo">
       <SearchForm @formSubmit="redirect"/>
     </div>
 
@@ -32,6 +38,29 @@ export default {
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+}
+
+.line {
+  background: rgba(178, 245, 234, .55);
+}
+
+.line::after {
+  content: '';
+  position: absolute;
+  width: 4.5rem;
+  height: .5rem;
+  background: rgba(178, 245, 234, .55);
+}
+
+.line-top::after {
+  transform-origin: bottom right;
+  transform: rotate(90deg) translateX(100%);
+}
+
+.line-bottom::after {
+  transform-origin: top left;
+  transform: rotate(90deg) translateX(-100%);
+  margin-left: .5rem;
 }
 
 .logo {
