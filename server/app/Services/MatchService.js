1c9e2b8d7025cc9412d4639e22251d06960c000a
Valentin Kaelin <kalanehd@gmail.com>
2020-01-01 16:22:40 +0100

    fix: return old matchList from db if Riot API is down

--- server/app/Services/MatchService.js
+++ server/app/Services/MatchService.js
@@ -15,7 +15,12 @@ class MatchService {
     let alreadyIn = false
     let index = 0
     do {
-      let newMatchList = (await Jax.Matchlist.accountID(account.accountId, account.region, index)).matches
+      let newMatchList = await Jax.Matchlist.accountID(account.accountId, account.region, index)
+      // Error while fetching Riot API
+      if (!newMatchList) {
+        return matchList
+      }
+      newMatchList = newMatchList.matches
       matchList = [...matchList, ...newMatchList]
       alreadyIn = newMatchList.length === 0 || stopFetching(newMatchList)
       // If the match is made in another region : we stop fetching
