f06563cfed78b2dd406e75f98e12b1c68e43790b
Valentin Kaelin <kalanehd@gmail.com>
2020-02-10 21:11:29 +0100

    chore: add redis connection settings to env file

--- server/config/redis.js
+++ server/config/redis.js
@@ -32,9 +32,9 @@ module.exports = {
   |
   */
   local: {
-    host: '127.0.0.1',
-    port: 6379,
-    password: null,
+    host: Env.get('REDIS_HOST', '127.0.0.1'),
+    port: Env.get('REDIS_PORT', 6379),
+    password: Env.get('REDIS_PASSWORD', null),
     db: 0,
     keyPrefix: ''
   },
