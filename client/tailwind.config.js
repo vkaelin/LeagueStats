02996c888321cf917f4acf177baf35ed2ae0ff56
Valentin Kaelin <kalanehd@gmail.com>
2020-06-11 20:39:28 +0200

    chore: update client deps and use built in tailwindcss purgecss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -717,4 +717,11 @@ module.exports = {
   },
   corePlugins: {},
   plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './src/**/*.vue',
+      './public/**/*.html',
+    ]
+  }
 }
