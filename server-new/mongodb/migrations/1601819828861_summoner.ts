2b2f46d71cb5fe4dfda4dd7ea270b2b445dadaf4
Valentin Kaelin <kalanehd@gmail.com>
2020-10-16 22:17:46 +0200

    refactor: add working mongo migrations when ReplicaSet is on

--- server-new/mongodb/migrations/1601819828861_summoner.ts
+++ server-new/mongodb/migrations/1601819828861_summoner.ts
@@ -2,7 +2,7 @@ import BaseMigration from '@ioc:Mongodb/Migration'
 
 export default class SummonerMigration extends BaseMigration {
   public up (): void {
-    // this.createCollection('summoners')
+    this.createCollection('summoners')
     this.createIndex('summoners', 'puuid')
   }
 }
--- server-new/mongodb/migrations/1601819837856_detailed_match.ts
+++ server-new/mongodb/migrations/1601819837856_detailed_match.ts
@@ -2,7 +2,7 @@ import BaseMigration from '@ioc:Mongodb/Migration'
 
 export default class DetailedMatchMigration extends BaseMigration {
   public up (): void {
-    // this.createCollection('detailed_matches')
+    this.createCollection('detailed_matches')
     this.createIndex('detailed_matches', 'gameId')
   }
 }
