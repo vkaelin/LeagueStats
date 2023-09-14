e7e2b598befe468ae41fb7afd4eb2ce79cad0c83
Valentin Kaelin <kalanehd@gmail.com>
2023-09-14 22:00:15 +0200

    chore: update adonis app

--- server/env.ts
+++ server/env.ts
@@ -20,7 +20,7 @@ export default Env.rules({
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
-  NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 
   DB_CONNECTION: Env.schema.string(),
   PG_HOST: Env.schema.string({ format: 'host' }),
